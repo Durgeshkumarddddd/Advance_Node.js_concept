1255e5a2fcec5a5581db3893555cd5c73ee84a74
Durgeshkumarddddd <durgeshkumarddddd123@gmail.com>
2025-04-29 22:34:02 +0530

    first commit

---
+++ NPM_NodeModules/app.js
@@ -0,0 +1,9 @@
+import {num} from 'maths';
+ 
+// const {num} = require('maths')
+
+console.log(num);
+
+// Those module which is in the node_modules is called NPM module 
+// 1-> npm login,  
+// 2 -> npm publish,
